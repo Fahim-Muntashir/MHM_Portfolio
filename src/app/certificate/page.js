e32943423fa764ffef2d39812ca942dfd66e744d
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-10-19 11:36:50 +0600

    added certificate page'

--- src/app/certificate/page.js
+++ src/app/certificate/page.js
@@ -1,67 +1,42 @@
 import React from "react";
 import "./style.css";
 import Header from "../components/Header";
-const page = () => {
+
+const getCertificates = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/certificates", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Faild to fetch certificates");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading in  certificate:", error);
+    throw error;
+  }
+};
+const page = async () => {
+  const { certificate } = await getCertificates();
   return (
     <div>
       <Header></Header>
       <div class="img-container my-10">
-        <div class="box">
-          <div class="imgBx">
-            <img src="https://images.unsplash.com/photo-1579748138140-ce9ef2c32db1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
-          </div>
-          <div class="content">
-            <div>
-              <h2>Image Title</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-                accusamus molestias quidem iusto.
-              </p>
+        {certificate.map((cft) => (
+          <div class="box">
+            <div class="imgBx">
+              <img src={cft.imgLink} />
+            </div>
+            <div class="content">
+              <div>
+                <h2>{cft.title}</h2>
+                <p>{cft.subTitle}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="box">
-          <div class="imgBx">
-            <img src="https://images.unsplash.com/photo-1579639782539-15cc6c0be63f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
-          </div>
-          <div class="content">
-            <div>
-              <h2>Image Title</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-                accusamus molestias quidem iusto.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="imgBx">
-            <img src="https://images.unsplash.com/photo-1603984362497-0a878f607b92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
-          </div>
-          <div class="content">
-            <div>
-              <h2>Image Title</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-                accusamus molestias quidem iusto.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="imgBx">
-            <img src="https://images.unsplash.com/photo-1579310962131-aa21f240d986?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80" />
-          </div>
-          <div class="content">
-            <div>
-              <h2>Image Title</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-                accusamus molestias quidem iusto.
-              </p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
